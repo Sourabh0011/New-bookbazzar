89454b22d5f8097f0175f23aa33721e875803beb
rahulvishwakarma-coder <rahulvishwakarma88872@gmail.com>
2025-12-16 21:08:54 +0530

    Version 30

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,21 +13,74 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Book Bazzar",
-  description: "Buy, sell, and explore books at Book Bazzar",
+  metadataBase: new URL("https://www.bookbazzar.in"),
+
+  title: {
+    default: "Book Bazzar - Buy, Sell & Discover Books Online",
+    template: "%s | Book Bazzar",
+  },
+
+  description:
+    "Book Bazzar is an online marketplace to buy, sell, and discover new and used books at the best prices.",
+
+  keywords: [
+    "book bazzar",
+    "buy books online",
+    "sell books online",
+    "used books",
+    "second hand books",
+    "online book store",
+    "book marketplace",
+  ],
+
+  authors: [{ name: "Book Bazzar Team" }],
+  creator: "Book Bazzar",
+
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+      "max-video-preview": -1,
+    },
+  },
 
   openGraph: {
-    title: "Book Bazzar",
-    description: "Buy, sell, and explore books at Book Bazzar",
+    title: "Book Bazzar - Buy, Sell & Discover Books Online",
+    description:
+      "Buy and sell new or used books online. Discover best deals on Book Bazzar.",
+    url: "https://www.bookbazzar.in",
+    siteName: "Book Bazzar",
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Book Bazzar - Online Book Marketplace",
+      },
+    ],
+    locale: "en_US",
     type: "website",
   },
 
   twitter: {
     card: "summary_large_image",
-    title: "Book Bazzar",
-    description: "Buy, sell, and explore books at Book Bazzar",
+    title: "Book Bazzar - Buy, Sell & Discover Books Online",
+    description:
+      "Buy and sell books online at the best prices on Book Bazzar.",
+    images: ["/og-image.jpg"],
   },
+
+  alternates: {
+    canonical: "https://www.bookbazzar.in",
+  },
+
+  category: "Books",
 };
+
 
 
 export default function RootLayout({
